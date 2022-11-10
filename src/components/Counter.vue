b1fabcaf86bd01bed01b2be4e222620cff998a3c
Battal Şahin <73367836+vbattalshn@users.noreply.github.com>
2022-11-10 17:45:54 +0300

    Add files via upload

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="counter flex flex-column flex-start flex-g-1">
-    <h1 class="counter-title" v-text="title ? title : null"/>
+    <h1 class="counter-title" v-text="title ? title : null" />
     <div class="counter-content-hero flex flex-g-2">
       <span class="counter-content flex flex-column flex-start">
-        <span class="counter-int" v-text="days"/>
+        <span class="counter-int" v-text="days" />
         <span class="counter-type">Gün</span>
       </span>
       <span class="counter-content flex flex-column flex-start">
-        <span class="counter-int" v-text="hours"/>
+        <span class="counter-int" v-text="hours" />
         <span class="counter-type">Saat</span>
       </span>
       <span class="counter-content flex flex-column flex-start">
-        <span class="counter-int" v-text="minutes"/>
+        <span class="counter-int" v-text="minutes" />
         <span class="counter-type">Dakika</span>
       </span>
       <span class="counter-content flex flex-column flex-start">
-        <span class="counter-int" v-text="seconds"/>
+        <span class="counter-int" v-text="seconds" />
         <span class="counter-type">Saniye</span>
       </span>
     </div>
@@ -24,48 +24,44 @@
 
 <script>
 export default {
-    props: ['title', 'name'],
-    data(){
-        return{
-            time : null,
-            days: null,
-            hours: null,
-            minutes: null,
-            seconds: null
-        }
-    },
-    mounted() {
-
-        var data = {
-        "session-id": localStorage.getItem("session"),
-        };
-
-        this.axios
-        .post(this.$store.state.api + "?action=get-" + this.name + "-remaining-time", JSON.stringify(data))
-        .then((response) => {
-            this.time = response.data.remaining_time;
-            this.secondsToDhms();
-            setInterval(() => {
-                this.time--;
-                this.secondsToDhms();
-            }, 1000);
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-    },
-    methods: {
-        secondsToDhms() {
-            var seconds = Number(this.time);
-            this.days = Math.floor(seconds / (3600*24));
-            this.hours = Math.floor(seconds % (3600*24) / 3600);
-            this.minutes = Math.floor(seconds % 3600 / 60);
-            this.seconds = Math.floor(seconds % 60);
-        }
+  props: ["title", "name"],
+  data() {
+    return {
+      time: null,
+      days: null,
+      hours: null,
+      minutes: null,
+      seconds: null,
+    };
+  },
+  computed: {
+    Rtime(){
+        return this.$store.state[this.name]
     }
-}
+  },
+  mounted() {
+    console.log(this.mktime(this.Rtime.hour, this.Rtime.minute, this.Rtime.month, this.Rtime.day, this.Rtime.year));
+    this.time = this.mktime(this.Rtime.hour, this.Rtime.minute, this.Rtime.month, this.Rtime.day, this.Rtime.year) - Math.round((new Date()).getTime()/1000);
+    this.secondsToDhms();
+    setInterval(() => {
+        this.time--;
+        this.secondsToDhms();
+    }, 1000);
+
+  },
+  methods: {
+    secondsToDhms() {
+      var seconds = Number(this.time);
+      this.days = Math.floor(seconds / (3600 * 24));
+      this.hours = Math.floor((seconds % (3600 * 24)) / 3600);
+      this.minutes = Math.floor((seconds % 3600) / 60);
+      this.seconds = Math.floor(seconds % 60);
+    },
+    mktime(hour, minute, month, day, year) {
+        return new Date(year, month - 1, day, hour, minute, 0).getTime() / 1000;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    api: "https://general-api.test/timelefttoyks/"
+    api: "https://general-api.test/timelefttoyks/",
+    yks: {
+      hour: 10,
+      minute: 15,
+      month: 6,
+      day: 17,
+      year: 2023,
+    },
+    msu: {
+      hour: 10,
+      minute: 15,
+      month: 3,
+      day: 26,
+      year: 2023,
+    },
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
