00344969b4b8936476ec4a1c50f80563183b6500
Battal Şahin <73367836+vbattalshn@users.noreply.github.com>
2023-01-13 18:02:22 +0300

    Update index.js
    Exam dates updated

--- src/store/index.js
+++ src/store/index.js
@@ -11,18 +11,18 @@ export default createStore({
       title: "YKS 2023'e kalan zaman"
     },
     lgs: {
-      hour: 10,
+      hour: 9,
       minute: 15,
       month: 6,
       day: 6,
       year: 2023,
-      title: "YKS 2023'e kalan zaman"
+      title: "LGS 2023'e kalan zaman"
     },
     msu: {
       hour: 10,
       minute: 15,
       month: 3,
-      day: 26,
+      day: 19,
       year: 2023,
       title: "MSÜ 2023'e kalan zaman"
     },
