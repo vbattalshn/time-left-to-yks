f8d74c12f2a937656e849b7c32ea26050e740d41
Battal Şahin <73367836+vbattalshn@users.noreply.github.com>
2023-07-11 23:45:42 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="counters flex flex-column flex-g-2">
     <Counter title="YKS 2023'e son" name="yks" />
+    <Counter title="MSÜ 2023'e son" name="msu" />
     <Counter title="LGS 2023'e son" name="lgs" />
   </div>
 </template>
